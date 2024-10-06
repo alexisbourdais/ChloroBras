e823effd82147244314ed467fc91a1ce57308a77
alexisbourdais <108393526+alexisbourdais@users.noreply.github.com>
2024-10-06 18:54:46 +0200

    Delete LaunChloroBras.sh

--- LaunChloroBras.sh
+++
@@ -1,9 +0,0 @@
-#!  /usr/bin/bash
-
-. /local/env/envnextflow-23.10.0.sh
-
-nextflow run ChloroBras.nf \
---formatReadName .fq.gz \
---workflow assembling \
--profile slurm,singularity \
---assembler getorganelle
